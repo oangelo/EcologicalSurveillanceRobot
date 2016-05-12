bee6b104f47551396f788319ea1dd0c103235afd
luccasbeckreis <luccasbeckreis@hotmail.com>
2016-05-12 19:35:05 -0300

    Corrected hexapod class header, setting up individually each leg

--- DemoWalking/hexapod.h
+++ DemoWalking/hexapod.h
@@ -6,10 +6,16 @@
 
 class Hexapod{
   public:
-      Hexapod(Leg first_left, Leg second_left, Leg third_left, Leg first_right, Leg second_right, Leg third_right);
+      Hexapod();
+      void set_leg_first_left(int coxa, int femur, int tibia);
+      void set_leg_second_left(int coxa, int femur, int tibia);
+      void set_leg_third_left(int coxa, int femur, int tibia);
+      void set_leg_first_right()int coxa, int femur, int tibia;
+      void set_leg_second_right(int coxa, int femur, int tibia);
+      void set_leg_third_right(int coxa, int femur, int tibia);
       void Init();
   private:
-      Leg[6];
+      Leg* leg_first_left, leg_first_right, leg_second_left, leg_second_right, leg_third_left, leg_third_right;
 };
 
 #endif
