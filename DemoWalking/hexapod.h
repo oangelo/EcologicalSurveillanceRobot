f3b1a1c867ba48731325f5e79778e8a43aed4634
luccasbeckreis <luccasbeckreis@hotmail.com>
2016-05-12 18:54:53 -0300

    Creation of Hexapod's class header.

---
+++ DemoWalking/hexapod.h
@@ -0,0 +1,13 @@
+#ifndef HEXAPOD_H
+#define HEXAPOD_H
+
+#include "leg.h"
+#include <Arduino.h>
+
+class Hexapod{
+  public:
+      Hexapod(Leg first_left, Leg second_left, Leg third_left, Leg first_right, Leg second_right, Leg third_right);
+      void Init();
+  private:
+      
+}
