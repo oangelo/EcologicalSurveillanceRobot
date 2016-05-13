0be323bee0d9a95af15fe95f9bf58d737bae1571
oangelo <oangelo@gmail.com>
2016-05-13 16:31:20 +0000

    Correcting leg's class

--- DemoWalking/DemoWalking.ino
+++ DemoWalking/DemoWalking.ino
@@ -8,11 +8,19 @@ void setup()
     leg = new Leg(2,3,4);
     //leg->set_spd(100);
     //leg->set_height(2);
-    //leg->set_length(2.5);
+    //leg->set_length(2);
     //leg->set_coxa_angle(70);
 }
 
 void loop()
 {
-
+   leg->set_height(2);
+   leg->set_length(2);
+   leg->move();
+   delay(100);
+   leg->set_height(2.3);
+   leg->set_length(2.3);
+   leg->move();
+   delay(100);
+   Serial.println("oi");
 }
--- DemoWalking/leg.cpp
+++ DemoWalking/leg.cpp
@@ -2,13 +2,14 @@
 #include <math.h>
 
 Leg::Leg(int p_coxa, int p_femur, int p_tibia):
-height(90), length(90), speed(200), coxa_angle(90), coxa(), femur(), tibia()
+height(2), length(2), speed(200), coxa_angle(90), coxa(), femur(), tibia()
 {
-	set_angles(this->height, this->length);
 
 	coxa.attach(p_coxa);
 	femur.attach(p_femur);
 	tibia.attach(p_tibia);
+  
+  set_angles(this->height, this->length);
 
 	move();
 }
@@ -25,14 +26,15 @@ void Leg::set_height(float height){
 void Leg::set_angles(float length, float height)
 {
 	//Both l1 and l2 are the leg's length//
-	float theta1, theta2, l1, l2;
+	float theta1, theta2, l1=2, l2=2;
   float x = length;
   float y = height;
 	theta1 = M_PI/2 +(sqrt(pow(l2,2.0) - pow(x,2.0) +2*x*l1-pow(l1,2.0)) - y) / l1;
 	theta2 = asin((x - l1)/l2);
 
-	this->femur_angle = theta1;
-	this->tibia_angle = theta2;
+	this->femur_angle = 180*theta1/M_PI;
+	this->tibia_angle = 180*theta2/M_PI;
+ Serial.println(theta1);
 }
 
 void Leg::set_coxa_angle(float coxa_angle)
@@ -46,7 +48,7 @@ void Leg::set_length(float length){
 }
 
 void Leg::move(){
-  femur.write(femur_angle);
-	tibia.write(tibia_angle);
-	coxa.write(coxa_angle);
+  femur.write(int(femur_angle));
+	tibia.write(int(tibia_angle));
+	coxa.write(int(coxa_angle));
 }
